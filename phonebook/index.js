5607a9208bee295fbbf58c26292ce45ed684db66
T7Q <tkuumola@gmail.com>
2020-11-14 19:56:35 +0200

    feat: :zap: part3 ex.3.5

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const nodemon = require('nodemon');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     {
         name: 'Ada Lovelace',
@@ -46,6 +48,21 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter((person) => person.id !== id);
 
     response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    
+    if (!body.name || !body.number){
+        return response.status(400).json({error: "No empty fiels allowed"});
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 10000)
+    }
+    persons = persons.concat(person);
+    response.json(person);
 });
 
 const PORT = 3001;
