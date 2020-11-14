a8fe054a60a444bd01de6297a5c0bdd1b847d045
T7Q <tkuumola@gmail.com>
2020-11-14 19:01:34 +0200

    feat: :zap: part3 ex. 3.1

---
+++ phonebook/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+    {
+      "name": "Ada Lovelace",
+      "number": "24-224-222",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Ada Loverance",
+      "number": "34",
+      "id": 8
+    }
+  ]
+  
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
