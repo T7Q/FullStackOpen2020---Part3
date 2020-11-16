00ee70572472cd94f1c997a679802137b7a6a310
T7Q <tkuumola@gmail.com>
2020-11-16 11:58:02 +0200

    feat: :zap: part3 ex. 3.14

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ morgan.token('body', function (req, res) {
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
-// app.use(express.static('build'));
+app.use(express.static('build'));
 
 app.get('/info', (request, response) => {
     response.send(`
@@ -28,13 +28,15 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+    // const id = Number(request.params.id);
+    // const person = persons.find((person) => person.id === id);
+    // if (person) {
+    Contact.find(id).then((contact) => {
+        response.json(contact);
+    });
+    // } else {
+    //     response.status(404).end();
+    // }
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -47,18 +49,16 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: 'Name or number missing' });
-    } else if (persons.map((person) => person.name).includes(body.name)) {
-        return response.status(400).json({ error: 'Name must bre unique' });
-    }
-    const person = {
+    // if (!body.name || !body.number) {
+    //     return response.status(400).json({ error: 'Name or number missing' });
+    // } else if (persons.map((person) => person.name).includes(body.name)) {
+    //     return response.status(400).json({ error: 'Name must bre unique' });
+    // }
+    const contact = new Contact({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * 10000),
-    };
-    persons = persons.concat(person);
-    response.json(person);
+    });
+    contact.save().then((savedContact) => response.json(savedContact));
 });
 
 const PORT = process.env.PORT;
